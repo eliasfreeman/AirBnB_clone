9c932715272117e6635100109975c4d8fad61a65
eliasfreeman <oladimejiboluwaji@gmail.com>
2022-10-29 12:35:00 +0100

    new

---
+++ models/base_model.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+import models
+import uuid
+from datetime import datetime
+
+"""Base Model Class"""
+
+
+class BaseModel:
+    """
+        BaseModel Class containing public instance attributes and methods
+        This Class defines all common attributes/methods for other classes
+    """
+    def __init__(self, *args, **kwargs):
+        """Re-creates an instance"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
+
+    def __str__(self):
+        """Prints in the form: [<class name>] (<self.id>) <self.__dict__>"""
+        class_name = self.__class__.__name__
+        tmp = '[{}] ({}) {}'
+        return tmp.format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """
+            Updates the public instance attribute (updated_at) with
+            current datetime
+        """
+        self.updated_at = datetime.now()
+        models.storage.save()
+        return
+
+    def to_dict(self):
+        """
+            Return a dictionary containing all keys/values of the instance
+            of __dict__ of the instance with a __class__ entry and
+            iso-formatted  datetime
+        """
+        new_dict = self.__dict__.copy()
+        new_dict.update({'__class__': str(self.__class__.__name__)})
+        new_dict["created_at"] = self.created_at.isoformat()
+        new_dict["updated_at"] = self.updated_at.isoformat()
+        return new_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+    File storage module
+"""
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage():
+    """
+        This module handles file storage
+        It serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = 'file.json'
+    __objects = {}
+
+    def all(self):
+        """
+            Returns the "__objects" dictionary
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+            Sets in "__objects" the "obj" with key "<obj class_name>.id"
+        """
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """
+            Serialize "__objects" and save to json file (path: __file_path)
+        """
+        obj_dict = {}
+
+        for key, value in self.__objects.items():
+            obj_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+            Deserialize the json file to "__objects" only if
+            "__file_path" exists. Otherwise, do nothing
+        """
+        try:
+            with open(self.__file_path, 'r') as f:
+                dict_obj = json.load(f)
+            for key, value in dict_obj.items():
+                self.__objects[key] = eval(key.split(".")[0])(**value)
+        except FileNotFoundError:
+            pass
